b6ad13e82a13b983c7b35b3a31321a5e1c240eb2
DarkMaster964 <nemanjavojnov@gmail.com>
2020-08-05 21:08:01 +0200

    Added subtraction function.

--- Test123.py
+++ Test123.py
@@ -2,10 +2,12 @@ def add_nums(num1, num2):
     sum = num1 + num2
     print("Sum is ",sum)
 
+def sub_num (num1, num2):
+    sub =  num1 - num2
+    print ("Sub is ", sub)
+
 def main():
     add_nums (5,10)
-
-
 
 if __name__ == "__main__":
     main()
