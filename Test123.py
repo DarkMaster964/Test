0d494c88d547b0bedd276d9ddca2d8827b49dbc0
DarkMaster964 <nemanjavojnov@gmail.com>
2020-08-05 21:21:43 +0200

    Added introducing function.

--- Test123.py
+++ Test123.py
@@ -6,8 +6,12 @@ def sub_num (num1, num2):
     sub =  num1 - num2
     print ("Sub is ", sub)
 
+def print_name(name):
+    print("My name is ", name)
+
 def main():
     add_nums (5,10)
+    print_name("John")
 
 if __name__ == "__main__":
     main()
